385fe5827c2424777faa95e67136025b5713b7bc
Inception <yyang53@ncsu.edu>
2018-09-29 21:54:32 -0400

    finish the user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,6 +5,10 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user?(user)
+    user == current_user
+  end
+
   # Returns the current logged-in user (if any).
   def current_user
     if session[:user_id]
@@ -21,5 +25,16 @@ module SessionsHelper
   def log_out
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
